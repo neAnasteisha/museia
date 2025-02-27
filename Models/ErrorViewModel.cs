1412825262b1d194b7666fa4357772448ec8ddd3
neAnasteisha <nastyazilvova@gmail.com>
2025-02-27 10:44:09 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace museia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

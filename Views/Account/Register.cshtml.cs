52ba345b4ab942701e4558c2c2c28e2cfb8444d1
kupchak.ol <kupchak.ol@gmail.com>
2025-03-13 17:42:41 +0200

    Внесено зміни в інтерфейс,  створено сторінки входу та реєстрації

---
+++ Views/Account/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace museia.Views.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

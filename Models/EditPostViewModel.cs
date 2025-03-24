85c7e794a26c4577dedf32f9b694966dbe231e55
Роман Шмигельський <roma.shmyhelskyi@gmail.com>
2025-03-24 23:32:49 +0200

    Додав редагування дописів. Виправив деякі орфографічні помилки

---
+++ Models/EditPostViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace museia.Models
+{
+    public class EditPostViewModel
+    {
+        public uint PostID { get; set; }
+        public string? PostText { get; set; }
+        public PostTag? PostTag { get; set; }
+        public string? PostPhotoUrl { get; set; }
+        public IFormFile? PostPhoto { get; set; }
+    }
+}
